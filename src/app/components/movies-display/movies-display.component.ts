db03702a3c01fa0d969afb0dd4a15f38c1faa9b0
Aravindviswak <aravindachary34@gmail.com>
2024-08-23 16:54:52 +0530

    second commit

--- src/app/components/movies-display/movies-display.component.ts
+++ src/app/components/movies-display/movies-display.component.ts
@@ -8,29 +8,38 @@ import { MovieDataService } from 'src/app/services/movie-data.service';
   styleUrls: ['./movies-display.component.css'],
 })
 export class MoviesDisplayComponent implements OnInit {
-  movie_data:MovieType[]=[];
-  filter_data:MovieType[]=[];
-  constructor(private service:MovieDataService ){
+  movie_data: MovieType[] = [];
+  selected_movie: MovieType | null = null;
+  filter_data: MovieType[] = [];
+    // Add a property to store the selected movie
 
-  }
+  constructor(private service: MovieDataService) {}
+
   ngOnInit(): void {
-    
-    this.service.getMovie().subscribe(movie=>{
-      this.movie_data=movie;
-    })
-    this.filter_data=[...this.movie_data]
+    this.service.getMovie().subscribe((movie) => {
+      this.movie_data = movie;
+      this.filter_data = [...this.movie_data]; // Correct placement to initialize filter_data
+    });
   }
-  
-  
-  searchMovie(event:Event){
-    const inputElement=event.target as HTMLInputElement;
-    const name=inputElement.value;
 
-    if(name){
-      this.filter_data=this.movie_data.filter(m=>m.movie_name?.toLowerCase().includes(name.toLowerCase()))
-    }else{
-      this.filter_data=[...this.movie_data]
+  searchMovie(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const name = inputElement.value;
+
+    if (name) {
+      this.filter_data = this.movie_data.filter((m) =>
+        m.movie_name?.toLowerCase().includes(name.toLowerCase())
+      );
+    } else {
+      this.filter_data = [...this.movie_data];
     }
   }
 
+  selectMovie(movie: MovieType) {
+    this.selected_movie = movie;  // Set the selected movie when clicked
+  }
+
+  clearSelection() {
+    this.selected_movie = null;  // Clear the selection if needed
+  }
 }
--- src/app/services/movie-data.service.ts
+++ src/app/services/movie-data.service.ts
@@ -3,66 +3,72 @@ import { MovieType } from '../movie-type';
 import { Observable, of } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieDataService {
+  constructor() {}
 
-  constructor() { }
-
-  movie_data:MovieType[] =[
-
+  movie_data: MovieType[] = [
     {
-movie_id:1001,
-movie_name:"Deadpool",
-movie_description:'Movie of Deadpool & Wolverine',
-movie_ticket_price:50,
-movie_image: "https://m.media-amazon.com/images/M/MV5BNzRiMjg0MzUtNTQ1Mi00Y2Q5LWEwM2MtMzUwZDU5NmVjN2NkXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"
+      movie_id: 1001,
+      movie_name: 'Deadpool',
+      movie_description: 'Movie of Deadpool & Wolverine',
+      movie_ticket_price: 50,
+      movie_image:
+        'https://m.media-amazon.com/images/M/MV5BNzRiMjg0MzUtNTQ1Mi00Y2Q5LWEwM2MtMzUwZDU5NmVjN2NkXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
     },
     {
-      movie_id:1002,
-      movie_name:"Kalki",
-      movie_description:'Movie of Kalki2829',
-      movie_ticket_price:70,
-      movie_image: "https://m.media-amazon.com/images/M/MV5BMTM3ZGUwYTEtZTI5NS00ZmMyLTk2YmQtMWU4YjlhZTI3NjRjXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg"
-          },
+      movie_id: 1002,
+      movie_name: 'Kalki',
+      movie_description: 'Movie of Kalki2829',
+      movie_ticket_price: 70,
+      movie_image:
+        'https://m.media-amazon.com/images/M/MV5BMTM3ZGUwYTEtZTI5NS00ZmMyLTk2YmQtMWU4YjlhZTI3NjRjXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+    },
 
-          {
-            movie_id:1003,
-            movie_name:"Bahubali",
-            movie_description:'Movie of Bahubali the begining',
-            movie_ticket_price:80,
-            movie_image: "https://m.media-amazon.com/images/M/MV5BYWVlMjVhZWYtNWViNC00ODFkLTk1MmItYjU1MDY5ZDdhMTU3XkEyXkFqcGdeQXVyODIwMDI1NjM@._V1_FMjpg_UX1000_.jpg"
-                },
-                {
-                  movie_id:1004,
-                  movie_name:"Jalsa",
-                  movie_description:'Movie of Jalsa',
-                  movie_ticket_price:10,
-                  movie_image: "https://m.media-amazon.com/images/M/MV5BMzBhNDk0ZTgtYTI5My00MGU2LWEzNDAtOGRhMTk3ZDdjMjE0XkEyXkFqcGdeQXVyNjQ1MDcxNzM@._V1_FMjpg_UX1000_.jpg"
-                      },
-                      {
-                        movie_id:1005,
-                        movie_name:"Devara",
-                        movie_description:'Movie of Devara',
-                        movie_ticket_price:40,
-                        movie_image: "https://m.media-amazon.com/images/M/MV5BYmYyYzZkZTEtOGNjYy00ZWUyLWFiYzYtYjViMzU1ZDk4ODhiXkEyXkFqcGdeQXVyNzI4NjYyMzk@._V1_.jpg"
-                            },
+    {
+      movie_id: 1003,
+      movie_name: 'Bahubali',
+      movie_description: `In the kingdom of Mahishmati, Shivudu falls in love with a young warrior woman. While trying to woo her, he learns about the conflict-ridden past of his family and his true legacy 
+                      Release date: 10 July 2015 (India)
+                      Director: S. S. Rajamouli
+                      Producers: Shobu Yarlagadda, Kovelamudi Raghavendra Rao, Prasad Devineni
+                      Budget: 180 crores INR (2015)
+                      Languages: Telugu, Tamil
+                      Sequel: Baahubali 2: The Conclusion
+`,
+      movie_ticket_price: 80,
+      movie_image:
+        'https://m.media-amazon.com/images/M/MV5BYWVlMjVhZWYtNWViNC00ODFkLTk1MmItYjU1MDY5ZDdhMTU3XkEyXkFqcGdeQXVyODIwMDI1NjM@._V1_FMjpg_UX1000_.jpg',
+    },
+    {
+      movie_id: 1004,
+      movie_name: 'Jalsa',
+      movie_description: 'Movie of Jalsa',
+      movie_ticket_price: 10,
+      movie_image:
+        'https://m.media-amazon.com/images/M/MV5BMzBhNDk0ZTgtYTI5My00MGU2LWEzNDAtOGRhMTk3ZDdjMjE0XkEyXkFqcGdeQXVyNjQ1MDcxNzM@._V1_FMjpg_UX1000_.jpg',
+    },
+    {
+      movie_id: 1005,
+      movie_name: 'Devara',
+      movie_description: 'Movie of Devara',
+      movie_ticket_price: 40,
+      movie_image:
+        'https://m.media-amazon.com/images/M/MV5BYmYyYzZkZTEtOGNjYy00ZWUyLWFiYzYtYjViMzU1ZDk4ODhiXkEyXkFqcGdeQXVyNzI4NjYyMzk@._V1_.jpg',
+    },
 
-                            {
-                              movie_id:1006,
-                              movie_name:"Pushpa",
-                              movie_description:'Movie of Pushpa',
-                              movie_ticket_price:47,
-                              movie_image: "https://m.media-amazon.com/images/M/MV5BNGZlNTFlOWMtMzUwNC00ZDdhLTk0MWUtOGZjYzFlOTBmNDdhXkEyXkFqcGdeQXVyMTUyNjIwMDEw._V1_.jpg"
-                                  },
+    {
+      movie_id: 1006,
+      movie_name: 'Pushpa',
+      movie_description: 'Movie of Pushpa',
+      movie_ticket_price: 47,
+      movie_image:
+        'https://m.media-amazon.com/images/M/MV5BNGZlNTFlOWMtMzUwNC00ZDdhLTk0MWUtOGZjYzFlOTBmNDdhXkEyXkFqcGdeQXVyMTUyNjIwMDEw._V1_.jpg',
+    },
+  ];
 
-
-
-  ]
-
-  getMovie():Observable<MovieType[]>{
+  getMovie(): Observable<MovieType[]> {
     return of(this.movie_data);
   }
- 
-  
 }
